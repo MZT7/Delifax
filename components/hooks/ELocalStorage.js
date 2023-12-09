e23b2f7eae69f8429febd5d90b4d1b0f1a43bf35
MZT7 <40639877+MZT7@users.noreply.github.com>
2023-12-09 01:12:29 +0100

    Update ELocalStorage.js

--- components/hooks/ELocalStorage.js
+++ components/hooks/ELocalStorage.js
@@ -3,16 +3,14 @@ import { useState, useEffect } from "react";
 
 const ELocalStorage = (key) => {
   // const [data, setData] = useState("");
-  let data;
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const value = localStorage.getItem(key);
-      data = JSON.parse(value);
-      // setData(JSON.parse(value))
-    }
-  }, [key]);
-
-  return data;
+  // let data;
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const value = localStorage.getItem(key);
+  //     data = JSON.parse(value);
+  //     // setData(JSON.parse(value))
+  //   }
+  // }, [key]);
+  // return data;
 };
 export default ELocalStorage;
